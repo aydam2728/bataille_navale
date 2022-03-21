635eee7d3018cb429cd5e3b01bca0457736f8660
aydam2728 <lesdmart@gmail.com>
2022-03-21 17:41:17 +0100

    site web

--- web/test.php
+++ web/test.php
@@ -1,14 +1,15 @@
 <!DOCTYPE html>
-<html lang="fr">
+<html lang="en">
 <head>
-  <title>Accueil - Bataille Navale</title>
+  <title>Bootstrap Example</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
   
 </head>
 <body>
-<nav class="navbar navbar-inverse" style="background-color: #0066cc !important; color:white !important; padding-bottom: 2%; font-size: 25px">
+
+<nav class="navbar navbar-inverse">
   <div class="container-fluid">
     <div class="navbar-header">
       <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
@@ -16,22 +17,29 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>                        
       </button>
-      <a href="#" class="navbar-brand">Logo</a>
+      <a class="navbar-brand" href="#">WebSiteName</a>
     </div>
     <div class="collapse navbar-collapse" id="myNavbar">
       <ul class="nav navbar-nav">
-        <li><a href="#">Règles</a></li>
-      <li><a href="#">Histoire du jeu</a></li>
-      <li><a href="#">Conception</a></li>
-      <li><a href="#">Manuel d’utilisation</a></li>
-      <li><a href="#">Contact</a></li>
+        <li class=""><a href="#">Home</a></li>
+        <li class="">
+          <a href="#">Page 1</a>
+        </li>
+        <li><a href="#">Page 2</a></li>
+        <li><a href="#">Page 3</a></li>
       </ul>
       <ul class="nav navbar-nav navbar-right">
-        <li><a href="#"><span class=" "></span> Jouer</a></li> 
+        <li><a href="#"><span class=""></span> Jouer</a></li>
       </ul>
     </div>
   </div>
 </nav>
+  
+<div class="container">
+  <h3>Collapsible Navbar</h3>
+  <p>In this example, the navigation bar is hidden on small screens and replaced by a button in the top right corner (try to re-size this window).
+  <p>Only when the button is clicked, the navigation bar will be displayed.</p>
+</div>
+
 </body>
 </html>
-
